b0c1f3c3376e93ecaee935d117db96e88e14b74f
Keshav <keshav162001@gmail.com>
2021-08-11 14:35:41 +0530

    added

---
+++ dice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Randomnumbergenerator());
+
+            Console.ReadKey();
+
+        }
+        public class Randomnumbergenerator() {
+            int face1;
+            int face2;
+
+                face1 = random.Next(1, 7);
+
+                face2 = random.Next(1, 7);
+            return face1 + face2 ;
+
+            }
+
+
+        }
+
+
+    }
+}
+    
