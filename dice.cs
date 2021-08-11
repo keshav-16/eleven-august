b241330e500ede7b2c0e44f380f19162c102d491
Keshav <keshav162001@gmail.com>
2021-08-11 18:06:15 +0530

    whole code

--- dice.cs
+++ dice.cs
@@ -1,31 +1,40 @@
 ﻿using System;
 
-namespace dice
+namespace diceroll
 {
     class Program
     {
-        static void Main(string[] args)
+        public class Randomnumbergenerator
         {
-            Console.WriteLine(Randomnumbergenerator());
-
-            Console.ReadKey();
-
-        }
-        public class Randomnumbergenerator() {
-            int face1;
-            int face2;
-
-                face1 = random.Next(1, 7);
-
-                face2 = random.Next(1, 7);
-            return face1 + face2 ;
-
+            int topSide;
+            int Sides = 6;
+            Random random = new Random();
+            public void setNum()
+            {
+                topSide = (random.Next(6) + 1);
+               
+            }
+            public int getNum()
+            {
+                return topSide;
             }
 
-
         }
-
-
+        static void Main(string[] args)
+        {
+            Randomnumbergenerator x = new Randomnumbergenerator();
+           int num = x.getNum();
+            int sum=0 ;
+            int[] numarr = new int[2];
+            Console.WriteLine("the random number is " + num);
+            numarr[0] = x.getNum();
+            numarr[1] = x.getNum();
+            for (int i=0; i<2; i++)
+            {
+                sum += numarr[i];
+            }
+            Console.WriteLine("the total of the two dice is " + sum);
+            Console.ReadLine();
+        }
     }
 }
-    
